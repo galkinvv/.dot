56731eb1c7ac971075644bdadf785251c815fe34
Vasily Galkin <galkin.vv@yandex.kz>
2022-05-18 03:55:02 +0300

    Add files via upload

--- multiseat/create-seat1.sh
+++ multiseat/create-seat1.sh
@@ -3,14 +3,21 @@ sleep 5
 #Note attaching bootup card0 to seat1 leads to problems with xorg that tries to enumerate pcie devices.
 #At least when card0=boot vga=NVIDIA, card1=second vga=AMD
 CARD=card1
-loginctl attach seat1 /sys/$(dirname $(udevadm info -q path /dev/dri/$CARD))/$CARD
+cmd=`echo loginctl attach seat1 /sys$(dirname $(udevadm info -q path /dev/dri/$CARD))`
+echo $cmd/card*
+$cmd/card*
+echo $cmd/render*
+$cmd/render*
 sleep 2
 
 #attach usb devices by vendor id
 for dev in /dev/input/by-id/*SIGMACH1P* /dev/input/by-id/*COMPANY*; do
 #for dev in /dev/input/by-id/*ROCCAT*; do
-	sleep 2
-	loginctl attach seat1 /sys/"$(dirname "$(udevadm info -q path "$dev")")"
+	sleep 3
+	echo $dev
+	cmd=`echo loginctl attach seat1 /sys"$(dirname "$(udevadm info -q path "$dev")")"`
+        echo $cmd
+	$cmd
 done
 
 #loginctl attach seat1 /sys/devices/platform/i8042/serio1/input/input2 #for ps2 mouse
