423f6e1f785632391e1c3eb61c511870bc3d5d1e
Vasily Galkin <galkin.vv@yandex.kz>
2023-09-20 18:47:55 +0300

    Update utils.py - Args

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import sys
 class XorShift64PrngIter:
     MOD = 2 ** 64
     def __init__(self, initial_state = 1):
@@ -14,3 +15,14 @@ class XorShift64PrngIter:
         self.state ^= (self.state << 25) % self.MOD
         self.state ^= self.state >> 27
         return (self.state * 2685821657736338717) % self.MOD
+
+def main(cmdline=sys.argv):
+    class Args:
+        unparsed_params_left = list(cmdline[1:])
+
+        @classmethod
+        def _next_arg(cls, fallback_value=None):
+            return cls.unparsed_params_left.pop(0) if cls.unparsed_params_left else fallback_value
+
+    Args.dev = str(Args._next_arg("/dev/x"))
+
