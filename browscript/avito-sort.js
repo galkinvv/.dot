f9a616eceda60c6d51cb74c33cc4f3d7f5858534
Vasily Galkin <galkin.vv@yandex.kz>
2024-08-24 02:57:03 +0300

    Create avito-sort.js

---
+++ browscript/avito-sort.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        New script avito.ru
+// @namespace   Violentmonkey Scripts
+// @match       https://www.avito.ru/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 24/08/2024, 02:14:45
+// ==/UserScript==
+(function() {
+  const sort_order = new URLSearchParams(window.location.search).get("s");
+  const buttons = document.querySelector('ul[data-marker=pagination-button]');
+  buttons.querySelectorAll('a').forEach( (el) => {
+        const href = el.getAttribute('href');
+        el.setAttribute('href', href+"&s="+sort_order);
+    });
+})();
